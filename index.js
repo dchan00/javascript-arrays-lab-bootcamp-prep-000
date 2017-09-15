132598340a21e7993a834b20f2feb894234cd6b0
DANIEL CHAN <dchan513@msn.com>
2017-09-15 01:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittensNew = [...kittens, name]
+  return kittensNew
+}
+
+function prependKitten(name) {
+  var kittensNew = [name, ...kittens]
+  return kittensNew
+}
+
+function removeLastKitten() {
+  var kittensNew = kittens.slice(0, kittens.length-1)
+  return kittensNew
+}
+
+function removeFirstKitten() {
+  var kittensNew = kittens.slice(1)
+  return kittensNew
+}
